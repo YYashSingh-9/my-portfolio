525881e117e25d31a39dd70563f6a97eee7b00f8
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-18 04:52:36 -0700

    navbar Done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,11 @@
 import classes from "./Header.module.css";
 import img1 from "../assets/my-cartoon-Final/laughing.png";
-import { Box, Container } from "@mui/material";
+import img2 from "../assets/my-cartoon-Final/working.png";
+import img3 from "../assets/my-cartoon-Final/idea.png";
+import img4 from "../assets/my-cartoon-Final/crossFingers.png";
+import img5 from "../assets/my-cartoon-Final/ThumbsUp.png";
+
+import { AppBar, Box, Container, Tooltip, Paper } from "@mui/material";
 import { Grid } from "@mui/material";
 const Header = () => {
   return (
@@ -10,25 +15,67 @@ const Header = () => {
           Container
           spacing={1}
           sx={{
-            backgroundColor: "red",
-            marginRight: "10px",
-            paddingTop: "200px",
+            height: "100vh",
           }}
         >
-          <Grid item lg={1} sx={{ maxWidth: "500px", backgroundColor: "pink" }}>
-            <Box className={classes.Imgbox}>
-              <div>
-                <img src={img1} />
-              </div>
-              <div>
-                <img src={img1} />
-              </div>
-              <div>
-                <img src={img1} />
-              </div>
-              <div>
-                <img src={img1} />
-              </div>
+          <Grid item lg={1} md={1}>
+            <AppBar
+              position="static"
+              sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+            >
+              <Box className={classes.Navbar}>
+                <div className={classes.navIcon}>
+                  <Tooltip
+                    title={<h3 className={classes.tooltipDesignH3}>My Page</h3>}
+                  >
+                    <img src={img1} />
+                  </Tooltip>
+                </div>
+                <div className={classes.navIcon}>
+                  <Tooltip
+                    title={
+                      <h3 className={classes.tooltipDesignH3}>My Skills</h3>
+                    }
+                  >
+                    <img src={img5} />
+                  </Tooltip>
+                </div>
+                <div className={classes.navIcon}>
+                  <Tooltip
+                    title={<h3 className={classes.tooltipDesignH3}>My Work</h3>}
+                  >
+                    <img src={img2} />
+                  </Tooltip>
+                </div>
+                <div className={classes.navIcon}>
+                  <Tooltip
+                    title={
+                      <h3 className={classes.tooltipDesignH3}>
+                        My Information
+                      </h3>
+                    }
+                  >
+                    <img src={img4} />
+                  </Tooltip>
+                </div>
+                <div className={classes.navIcon}>
+                  <Tooltip
+                    title={
+                      <h3 className={classes.tooltipDesignH3}>
+                        My Contact info
+                      </h3>
+                    }
+                  >
+                    <img src={img3} />
+                  </Tooltip>
+                </div>
+              </Box>
+            </AppBar>
+          </Grid>
+          <Grid item lg={1} md={1} sm={1}>
+            <Box>
+              <h1>I'm Yash.</h1>
+              <h3>I change and create the future.</h3>
             </Box>
           </Grid>
         </Grid>
