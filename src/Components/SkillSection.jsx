445ca4c331afa9579d846b97385c41b4962b2490
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-18 08:10:34 -0700

    Web icons

--- src/Components/SkillSection.jsx
+++ src/Components/SkillSection.jsx
@@ -1,6 +1,16 @@
 import { Box, Grid } from "@mui/material";
 import classes from "./SkillSection.module.css";
-import icon1 from "../assets/icons/";
+import html from "../assets/icons/html.png";
+import express from "../assets/icons/express.png";
+import react from "../assets/icons/react.png";
+import css from "../assets/icons/css.png";
+import js from "../assets/icons/js.png";
+import mongodb from "../assets/icons/mongodb.png";
+import mui from "../assets/icons/mui.png";
+import mongoose from "../assets/icons/mongoose.png";
+import node from "../assets/icons/node.png";
+import jwt from "../assets/icons/jwt.png";
+
 const SkillSection = () => {
   return (
     <>
@@ -15,16 +25,28 @@ const SkillSection = () => {
           <Grid item>
             <Box className={classes.logos}>
               <div className={classes.logoSlide}>
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
-                <img />
+                <img src={html} />
+                <img src={css} />
+                <img src={js} />
+                <img src={react} />
+                <img src={express} />
+                <img src={mongodb} />
+                <img src={node} />
+                <img src={mongoose} />
+                <img src={jwt} />
+                <img src={mui} />
+              </div>
+              <div className={classes.logoSlide}>
+                <img src={html} />
+                <img src={css} />
+                <img src={js} />
+                <img src={react} />
+                <img src={express} />
+                <img src={mongodb} />
+                <img src={node} />
+                <img src={mongoose} />
+                <img src={jwt} />
+                <img src={mui} />
               </div>
             </Box>
           </Grid>
