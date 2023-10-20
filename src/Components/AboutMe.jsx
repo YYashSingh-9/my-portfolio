f8f9d3d6af57d108cf268cba8e8825970dfdcb53
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-20 03:04:34 -0700

    AboutUI

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -19,37 +19,41 @@ const AboutMe = () => {
               </Box>
             </Grid>
             <Grid item lg={9} md={9} sm={6}>
-              <Box className={classes.titleNtext}>
-                <h2>I wanna say this about me..</h2>
-                <span>
-                  <p>
-                    This will be short and crisp , i am a full stack web
-                    developer but getting a job is not the main cause of me
-                    being a developer .. First is i love tech and building stuff
-                    also i believe that this skill is power with this i can
-                    create products or services which can make a dent on this
-                    planet. <br />
-                    <br />
-                    As you might have already seen my skills section so you know
-                    what i know and use but apart from that i am ..
-                    <span className={classes.highlightedText}>
-                      more which makes me stand our from the crowd which are...
-                    </span>
-                    <br />
-                    <br />
-                    1. I am excellent in business planning and product planning.
-                    <br />
-                    2. I like to work on detailing like minute details also.
-                    <br />
-                    3. Real world problem solving and converting it into a
-                    product / service.
-                    <br />
-                    4. I take control and lead.
-                    <br />
-                    5. I am a very good in communication and management.
-                    <br />
-                  </p>
-                </span>
+              <Box>
+                <Box className={classes.titleNtext}>
+                  <h2>I wanna say this about me..</h2>
+                  <span>
+                    <p>
+                      This will be short and crisp , i am a full stack web
+                      developer but getting a job is not the main cause of me
+                      being a developer .. First is i love tech and building
+                      stuff also i believe that this skill is power with this i
+                      can create products or services which can make a dent on
+                      this planet. <br />
+                      <br />
+                      As you might have already seen my skills section so you
+                      know what i know and use but apart from that i am ..
+                      <span className={classes.highlightedText}>
+                        more which makes me stand our from the crowd which
+                        are...
+                      </span>
+                      <br />
+                      <br />
+                      1. I am excellent in business planning and product
+                      planning.
+                      <br />
+                      2. I like to work on detailing like minute details also.
+                      <br />
+                      3. Real world problem solving and converting it into a
+                      product / service.
+                      <br />
+                      4. I take control and lead.
+                      <br />
+                      5. I am a very good in communication and management.
+                      <br />
+                    </p>
+                  </span>
+                </Box>
                 <Box className={classes.extraLink}>
                   <h3>
                     <a href="https://www.linkedin.com/in/yashwardhanm-singh-083064291/">
